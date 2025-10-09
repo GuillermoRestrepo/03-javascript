50356cc27b2813ba3a9aa353a5a36da26f884917
GuillermoRestrepo <GuillermoRestrepoCode@gmail.com>
2025-10-09 15:38:39 -0500

    01-filename-conventions: Nombrar correctamente archivos de JavaScript.

---
+++ 01-filename-conventions.js
@@ -0,0 +1,11 @@
+/*
+        NOMBRAR ARCHIVOS DE JAVASCRIPT
+    Pueden contener:
+      · Letras de a-z (de preferencia minúsculas).
+      · Números de 0-9.
+      · Guiones - kebab-case (recomendado).
+      · Guiones bajos _ snake_case (Posible, pero no recomendado).
+    
+    Ejemplo:
+      · 00-mi-archivo.js
+*/
