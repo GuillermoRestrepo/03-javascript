ccec857c8c82ef751321050d9578d1a9e250d9ea
GuillermoRestrepo <GuillermoRestrepoCode@gmail.com>
2025-10-10 14:43:01 -0500

    02-use-strict: activar modo stricto en JavaScript.

---
+++ 02-use-strict.js
@@ -0,0 +1,13 @@
+/*
+        USE STRICT
+    Hace que escribir codigo javascript sea mas estricto, no permite errores que normalmente pasaría por alto.
+
+    Puede útilizarce en la primera línea del documento y aplicará el modo stricto en todo el documento.
+
+    Puede útilizarce en la primera línea dentro de una funcion, esto hace q dicha función tenga que ser escrita estrictamente bien.
+
+    Es aconsejable útilizarla en la primera línea del DOC.
+    (Así toca acostumbrarse a escribir bien el código).
+
+    "use strict" --> así se declara.
+*/
