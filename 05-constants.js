c37cf4a8df7ea0d1b850b8a2637a82856b6b6eda
GuillermoRestrepo <GuillermoRestrepoCode@gmail.com>
2025-10-12 15:42:13 -0500

    05-constants: definir constantes en JavaScript.

---
+++ 05-constants.js
@@ -0,0 +1,9 @@
+/*
+        CONSTANTES
+    La forma correcta de definir una constantes es utilizando:
+      · UPPER_SNAKE_CASE.
+      · Reemplazar let por const.
+*/
+
+const UNA_CONSTANTE= 3.1416;
+console.log(UNA_CONSTANTE);
