095472492ec28a7ef65f41034cf85efebc7ddc03
GuillermoRestrepo <GuillermoRestrepoCode@gmail.com>
2025-10-12 15:33:48 -0500

    04-variables: definir variables en JavaScript.

---
+++ 04-variables.js
@@ -0,0 +1,17 @@
+/*
+        VARIABLES
+    Las variables se definen:
+      · Deben de comenzar con una letra, un guión bajo o un signo de dolar.
+      · Permite letras, tanto mayúsculas como minúsculas.
+      · Permite números.
+      · Se utiliza camelCase
+*/
+
+/*
+        VAR - LET
+    var: declarar variables globales (NO RECOMENDADO).
+    let: declarar variables globales y de bloque (USAR SIEMPRE).
+*/
+
+let variable= "Esto es ua variable";
+console.log(variable);
